c0f7152c611a6580ff7f72dca95608fe557ee6a5
Alex Tang <alex.m.tang@gmail.com>
2013-11-09 17:58:02 -0500

    Modify README Add names

--- insert.py
+++ insert.py
@@ -1,13 +1,17 @@
 import MySQLdb
+import names
 
-conn = MySQLdb.connect(host= "cs336-23.cs.rutgers.edu",
-                user="csuser",
-                passwd="cs97f462",
-                db="csuser")
+conn = MySQLdb.connect(host="cs336-23.cs.rutgers.edu",
+                       user="csuser",
+                       passwd="cs97f462",
+                       db="csuser")
 
 curr = conn.cursor()
 
-curr.execute("""INSERT INTO stem VALUE (%s)""",(90))
-conn.commit()
+numrows = 1000
+
+for i in range(numrows):
+    curr.execute("INSERT INTO stem VALUE (%s)", names.get_full_name())
+    conn.commit()
 
 conn.close()
