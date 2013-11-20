b7e793079f67d5060867cafab65a99af8b88f115
Alex Tang <alex.m.tang@gmail.com>
2013-11-19 20:26:42 -0500

    Got night club names

--- db_generate/night_clubs.py
+++ db_generate/night_clubs.py
@@ -2,6 +2,24 @@ import random
 import cities
 from faker import Faker
 
+def generate_name():
+    adjs = open("../scrape/adjectives.txt", "r")
+    adjs_lines = adjs.readlines()
+    i = random.randint(0, len(adjs_lines))
+    rand_adj = adjs_lines[i].strip().capitalize()
+
+    animals = open("../scrape/animals.txt", "r")
+    animals_lines = animals.readlines()
+    i = random.randint(0, len(animals_lines))
+    rand_animal = animals_lines[i].strip().capitalize()
+
+    name = "The %s %s" % (rand_adj, rand_animal)
+
+    adjs.close()
+    animals.close()
+    return name
+
+
 class Night_Club:
     def __init__(self, name, addr, city, phone, license):
         self.name = name
@@ -14,7 +32,7 @@ class Night_Club:
     def generate():
         fake = Faker()
 
-        name = ""
+        name = generate_name()
         addr = fake.street_address()
         city = cities.get_random()
         phone = fake.random_number(digits=10)
